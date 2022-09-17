9a3e36ef147160a70f597585d01b9cb6e14ff462
Jeff Carbine <jcarbine@dhicorp.com>
2022-09-16 18:05:25 -0600

    adding in check for localhost

--- components/auth/auth-render.js
+++ components/auth/auth-render.js
@@ -3,5 +3,8 @@ import loggedIn from "./logged-in.js";
 export default (req, res, path, values = {}, status = 200) => {
   values.loggedIn = loggedIn(req);
 
+  const localhost = req.ip === "::ffff:127.0.0.1";
+  values.localhost = localhost;
+
   res.status(status).render(path, values);
 };
