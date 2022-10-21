c2d9d457480a7733a88dc55acfb45411fcbac82d
Jeff Carbine <jeff@carbine.co>
2022-10-21 13:56:39 -0600

    some minus sign icons

--- scripts/createDynamicElement/_createDynamicElement.js
+++ scripts/createDynamicElement/_createDynamicElement.js
@@ -1,3 +1,5 @@
+import iconList from "../../components/icon/_icon-list.js";
+
 /**
  * Create Dynamic Element
  * Allows you to better create HTML elements via Javascript with classes, ids and attributes
@@ -27,34 +29,48 @@ export const createDynamicElement = (obj) => {
 
   if (obj.tagName !== undefined) {
     element = document.createElement(obj.tagName);
+  } else if (obj.icon) {
+    element = "";
   } else {
     element = document.createElement("div");
   }
 
-  for (var key in obj) {
-    if (
-      key !== "children" &&
-      key !== "child" &&
-      key !== "tagName" &&
-      key !== "textContent"
-    ) {
-      element.setAttribute(key, obj[key]);
-    } else if (key === "textContent") {
-      element.textContent = obj[key];
-    } else if (key === "children") {
-      let children = obj[key];
+  if (obj.icon) {
+    // this is an icon, so we can ignore the rest
+    const iconName = obj.icon,
+      iconString = iconList[iconName],
+      markup =
+        "<svg xmlns='http://www.w3.org/2000/svg' id='Layer_1' data-name='Layer 1' viewBox='0 0 320.27 316.32'>" +
+        iconString.replace(/cls/g, iconName) +
+        "</svg>";
 
-      for (var i = 0; i < children.length; i++) {
-        let child = children[i];
+    element = new DOMParser().parseFromString(markup, "text/xml").firstChild;
+  } else {
+    for (var key in obj) {
+      if (
+        key !== "children" &&
+        key !== "child" &&
+        key !== "tagName" &&
+        key !== "textContent"
+      ) {
+        element.setAttribute(key, obj[key]);
+      } else if (key === "textContent") {
+        element.textContent = obj[key];
+      } else if (key === "children") {
+        let children = obj[key];
+
+        for (var i = 0; i < children.length; i++) {
+          let child = children[i];
+
+          let childElement = createDynamicElement(child);
+          element.appendChild(childElement);
+        }
+      } else if (key === "child") {
+        let child = obj[key];
 
         let childElement = createDynamicElement(child);
         element.appendChild(childElement);
       }
-    } else if (key === "child") {
-      let child = obj[key];
-
-      let childElement = createDynamicElement(child);
-      element.appendChild(childElement);
     }
   }
 
