442948e92028edd80215883e375b1fd65e902945
Jeff Carbine <jeffcarbine@gmail.com>
2023-01-24 17:00:20 -0700

    made cookie a module, moved weekRange into dateRange

--- scripts/dateRange/_dateRange.js
+++ scripts/dateRange/_dateRange.js
@@ -38,7 +38,7 @@ export const monthToDate = (start_date = new Date()) => {
 
 export const fullMonth = (start_date = new Date()) => {
   let startOfMonth = new Date(start_date),
-  endOfMonth = new Date(start_date);
+    endOfMonth = new Date(start_date);
 
   const month = start_date.getMonth();
 
@@ -47,4 +47,25 @@ export const fullMonth = (start_date = new Date()) => {
   endOfMonth.setDate(0);
 
   return [startOfMonth, endOfMonth];
-}
+};
+
+export const weekRange = (offsetWeeks = 0, date = new Date()) => {
+  const week_comparison = new Date(
+    date.getFullYear(),
+    date.getMonth(),
+    date.getDate() + offsetWeeks * 7
+  );
+
+  let start_date = new Date(),
+    end_date = new Date();
+
+  start_date.setDate(week_comparison.getDate() - week_comparison.getDay());
+  start_date.setHours(0, 0, 0, 0);
+  end_date.setDate(start_date.getDate() + 7);
+  end_date.setHours(23, 59, 59, 999);
+
+  return {
+    start_date,
+    end_date,
+  };
+};
