83efd0c052e656b0c048cb43c171c3c52e3874fd
Jeff Carbine <jeffcarbine@gmail.com>
2023-01-27 17:27:29 -0700

    beginning of filterable tables, and new and improved xhrInput

--- elements/table/_table.js
+++ elements/table/_table.js
@@ -1,19 +1,122 @@
-﻿function wrapTables() {
-  let tables = document.querySelectorAll("table:not(.wrapped)");
+﻿import { addEventDelegate } from "../../scripts/eventdelegate/_eventdelegate.js";
 
-  loop(tables, function(table) {
-    wrapTable(table);
-  });
+/**
+ * Create a function to sort the given table.
+ */
+function sortTableFunction(th) {
+  const table = th.closest("table");
+
+  sortRows(table, siblingIndex(th));
 }
 
-function wrapTable(table) {
-  let tableWrapper = document.createElement("div");
-  tableWrapper.classList.add("table-wrapper");
-  let parent = table.parentNode;
-  parent.insertBefore(tableWrapper, table);
-  tableWrapper.appendChild(table);
-  table.classList.add("wrapped");
+addEventDelegate("click", "table thead th", sortTableFunction);
+
+/**
+ * Get the index of a node relative to its siblings — the first (eldest) sibling
+ * has index 0, the next index 1, etc.
+ */
+function siblingIndex(node) {
+  var count = 0;
+
+  while ((node = node.previousElementSibling)) {
+    count++;
+  }
+
+  return count;
 }
 
-addEventDelegate("load", window, wrapTables);
-//addEventDelegate("childList", "table:not(.wrapped)", wrapTable);
+/**
+ * Sort the given table by the numbered column (0 is the first column, etc.)
+ */
+function sortRows(table, columnIndex) {
+  var rows = table.querySelectorAll("tbody tr"),
+    sel = "thead th:nth-child(" + (columnIndex + 1) + ")",
+    sel2 = "tbody tr *:nth-child(" + (columnIndex + 1) + ")",
+    classList = table.querySelector(sel).classList,
+    values = [],
+    cls = "",
+    allNum = true,
+    val,
+    index,
+    node;
+
+  if (classList) {
+    if (classList.contains("date")) {
+      cls = "date";
+    } else if (classList.contains("number")) {
+      cls = "number";
+    }
+  }
+
+  for (index = 0; index < rows.length; index++) {
+    node = rows[index].querySelector(sel2);
+    val = node.innerText;
+
+    if (isNaN(val)) {
+      allNum = false;
+    } else {
+      val = parseFloat(val);
+    }
+
+    values.push({ value: val, row: rows[index] });
+  }
+
+  if (cls == "" && allNum) {
+    cls = "number";
+  }
+
+  if (cls == "number") {
+    values.sort(sortNumberVal);
+    values = values.reverse();
+  } else if (cls == "date") {
+    values.sort(sortDateVal);
+  } else {
+    values.sort(sortTextVal);
+  }
+
+  for (var idx = 0; idx < values.length; idx++) {
+    table.querySelector("tbody").appendChild(values[idx].row);
+  }
+}
+
+/**
+ * Compare two 'value objects' numerically
+ */
+function sortNumberVal(a, b) {
+  return sortNumber(a.value, b.value);
+}
+
+/**
+ * Numeric sort comparison
+ */
+function sortNumber(a, b) {
+  return a - b;
+}
+
+/**
+ * Compare two 'value objects' as dates
+ */
+function sortDateVal(a, b) {
+  var dateA = Date.parse(a.value),
+    dateB = Date.parse(b.value);
+
+  return sortNumber(dateA, dateB);
+}
+
+/**
+ * Compare two 'value objects' as simple text; case-insensitive
+ */
+function sortTextVal(a, b) {
+  var textA = (a.value + "").toUpperCase();
+  var textB = (b.value + "").toUpperCase();
+
+  if (textA < textB) {
+    return -1;
+  }
+
+  if (textA > textB) {
+    return 1;
+  }
+
+  return 0;
+}
---
+++ scripts/xhr/_xhrInput.js
@@ -0,0 +1,87 @@
+import { addEventDelegate } from "../eventdelegate/_eventdelegate.js";
+import { xhr } from "./_xhr.js";
+
+const xhrInput = function (input) {
+  // first, find the label for the input
+  // and give it the class of "waiting"
+  const label =
+    input.parentNode.tagName === "LABEL"
+      ? input.parentNode
+      : document.querySelector("label[for=" + (input.id || input.name) + "]");
+
+  if (label !== null) {
+    label.classList.add("waiting");
+  }
+
+  const method = input.getAttribute("method"),
+    action = input.getAttribute("action"),
+    data = JSON.parse(input.dataset.xhrStaticValues),
+    name = input.name;
+
+  let value = // different for checkboxes
+    input.type === "checkbox" || input.type === "radio"
+      ? input.checked
+      : input.value;
+
+  // if this is a number input, parseInt if no period, parseFloat if period
+  if (input.type === "number") {
+    if (value.includes(".")) {
+      value = parseFloat(value);
+    } else {
+      value = parseInt(value);
+    }
+  }
+
+  data[name] = value;
+
+  console.log(data);
+
+  // default behaviours for success, error and failure
+  const success = function () {
+    if (label !== null) {
+      label.classList.remove("waiting");
+      label.classList.remove("error");
+      label.classList.remove("failure");
+      label.classList.remove("unsavedChanges");
+    }
+  };
+
+  const error = function (request) {
+    if (label !== null) {
+      label.classList.add("error");
+      label.classList.remove("waiting");
+    }
+  };
+
+  const failure = function (request) {
+    if (label !== null) {
+      label.classList.add("failure");
+      label.classList.remove("waiting");
+    }
+  };
+
+  // and now pass this all to the xhr function
+  xhr(method, action, data, { success, error, failure });
+};
+
+// run xhrForm on any form with a class of xhr
+addEventDelegate(
+  "change",
+  "input.xhrInput, textarea.xhrInput, select.xhrInput",
+  xhrInput,
+  true
+);
+
+// make the input have unsavedChanges
+const addUnsavedChanges = (input) => {
+  let label = input.parentNode;
+
+  label.classList.add("unsavedChanges");
+};
+
+addEventDelegate(
+  "input",
+  "input.xhrInput, textarea.xhrInput, select.xhrInput",
+  addUnsavedChanges,
+  true
+);
