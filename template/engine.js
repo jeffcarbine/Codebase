b2c90425eaa24aecfe4984683b68d2996ca6cb13
Jeff Carbine <jeffcarbine@gmail.com>
2023-01-30 10:53:11 -0700

    throwing in the example for good measure

--- template/engine.js
+++ template/engine.js
@@ -8,3 +8,25 @@ export const template_js_engine = (app) => {
     return callback(null, rendered);
   });
 };
+
+// const fs = require('fs') // this engine requires the fs module
+// app.engine('ntl', (filePath, options, callback) => { // define the template engine
+//   fs.readFile(filePath, (err, content) => {
+//     if (err) return callback(err)
+//     // this is an extremely simple template engine
+//     const rendered = content.toString()
+//       .replace('#title#', `<title>${options.title}</title>`)
+//       .replace('#message#', `<h1>${options.message}</h1>`)
+//     return callback(null, rendered)
+//   })
+// })
+// app.set('views', './views') // specify the views directory
+// app.set('view engine', 'ntl') // register the template engine
+
+// index.ntl
+// #title#
+// #message#
+
+// app.get('/', (req, res) => {
+//   res.render('index', { title: 'Hey', message: 'Hello there!' })
+// })
