b3f027639dd51dd171295ed64c36c2c07fa99727
Jeff Carbine <jeffcarbine@gmail.com>
2023-04-20 13:59:39 -0600

    some tweaks

--- scripts/formatString/formatString.js
+++ scripts/formatString/formatString.js
@@ -22,3 +22,11 @@ export const deCamelize = (str) => {
   const result = str.replace(/([A-Z])/g, " $1");
   return result.charAt(0).toUpperCase() + result.slice(1);
 };
+
+export const camelize = (str) => {
+  return str
+    .replace(/(?:^\w|[A-Z]|\b\w)/g, (word, index) => {
+      return index === 0 ? word.toLowerCase() : word.toUpperCase();
+    })
+    .replace(/[^A-Z0-9]+/gi, "");
+};
