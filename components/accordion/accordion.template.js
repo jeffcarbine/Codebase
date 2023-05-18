84c8c3f66571a80a04edfa8ecb6d070bf63d7e64
Jeff Carbine <jeffcarbine@gmail.com>
2023-05-18 00:34:45 -0600

    adding templates!

--- components/accordion/accordion.template.js
+++ components/accordion/accordion.template.js
@@ -1,7 +1,11 @@
 import * as e from "../../elements/elements.js";
 
-export const accordionTemplate = (title, content, className = "") => {
-  const accordionBody = content;
+export const accordionTemplate = ({
+  className = "",
+  title = "",
+  body = "",
+} = {}) => {
+  const accordionBody = body;
   accordionBody.class += " accordion-body";
 
   return {
