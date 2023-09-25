072bc73718515e269fd0af7e7d3108811fa0f6f2
Jeff Carbine <jeffcarbine@gmail.com>
2023-09-25 16:47:48 -0600

    update to modal scripts too

---
+++ components/modal/modal.scripts.js
@@ -0,0 +1,42 @@
+﻿import { MODAL } from "./modal.component.js";
+import { renderTemplate } from "/periodic/template/renderTemplate.js";
+import { addEventDelegate } from "../../modules/eventDelegate/eventDelegate.js";
+
+export const createModal = ({
+  modalBody,
+  sibling,
+  className = "",
+  id = "",
+} = {}) => {
+  const newModal = renderTemplate(MODAL({ modalBody, className, id }));
+
+  sibling.after(newModal);
+
+  newModal.showModal();
+
+  addEventDelegate("click", "dialog .close", destroyModal);
+};
+
+const destroyModal = (button) => {
+  const modal = button.parentNode;
+
+  modal.remove();
+};
+
+const closeModal = (button) => {
+  const modal = button.parentNode;
+
+  modal.close();
+};
+
+const openModal = (button) => {
+  const modalId = button.dataset.modal,
+    modal = document.querySelector("#" + modalId);
+
+  console.log(modalId, modal);
+
+  modal.showModal();
+};
+
+addEventDelegate("click", "dialog .close", closeModal, true);
+addEventDelegate("click", "[data-modal]", openModal, true);
