84d07761d639b426f477f38ca9b6732ea9638e71
Jeff Carbine <jeffcarbine@gmail.com>
2023-11-24 13:35:00 -0700

    changing throttle based on screen size

--- modules/eventDelegate/eventDelegate.js
+++ modules/eventDelegate/eventDelegate.js
@@ -144,9 +144,12 @@ window.addEventListener("load", function () {
 // array of functions that will be run on window scroll
 const scrollFunctions = [];
 
-// 120fps scroll throttling
+// scroll throttling
 let lastKnownScrollPosition = 0;
 let ticking = false;
+
+// refresh rate - 120fps for large screens, 30fps for small screens
+let refreshRate = window.innerWidth > 768 ? 8 : 33;
 
 // register the function to the
 document.addEventListener("scroll", function (e) {
@@ -160,7 +163,7 @@ document.addEventListener("scroll", function (e) {
       }
 
       ticking = false;
-    }, 8.33); // 120fps
+    }, refreshRate);
 
     ticking = true;
   }
