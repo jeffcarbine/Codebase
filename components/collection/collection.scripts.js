cbd9bd07cf7b7e4ce18b85e6c9d8db23ee82827f
Jeff Carbine <jeffcarbine@gmail.com>
2023-12-22 17:13:04 -0700

    new collection component

---
+++ components/collection/collection.scripts.js
@@ -0,0 +1,28 @@
+import { xhr } from "../../modules/xhr/xhr.js";
+import { renderTemplate } from "../../template/renderTemplate.js";
+import { PRODUCTSUMMARY } from "../product/productSummary.component.js";
+
+const productSummaryGrids = document.querySelectorAll(".productSummaryGrid");
+
+productSummaryGrids.forEach((productSummaryGrid) => {
+  const collectionHandle = productSummaryGrid.id;
+
+  const fetchCollectionSuccess = (request) => {
+    const products = JSON.parse(request.response).products;
+
+    // clear out the placeholders
+    productSummaryGrid.innerHTML = "";
+
+    products.forEach((product) => {
+      const productSummary = renderTemplate(PRODUCTSUMMARY({ data: product }));
+
+      productSummaryGrid.appendChild(productSummary);
+    });
+  };
+
+  xhr({
+    path: "/shop/collection",
+    body: { collectionHandle },
+    success: fetchCollectionSuccess,
+  });
+});
