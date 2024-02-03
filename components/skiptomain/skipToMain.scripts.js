21fa71f827640c7bafaf2e8ba578fb40656ea092
Jeff Carbine <jeffcarbine@gmail.com>
2024-02-03 16:32:28 -0700

    working on skiptomain

---
+++ components/skiptomain/skipToMain.scripts.js
@@ -0,0 +1,12 @@
+﻿import { addEventDelegate } from "../../modules/eventDelegate/eventDelegate.js";
+
+function skipToMain() {
+  let main = document.querySelector("main");
+  main.setAttribute("tabindex", "0");
+
+  setTimeout(() => {
+    main.focus();
+  }, 500);
+}
+
+addEventDelegate("click", "#skipToMain", skipToMain);
--- components/skiptomain/skiptomain.html.js
+++ components/skiptomain/skiptomain.html.js
@@ -1,3 +1,15 @@
 export class SKIPTOMAIN {
-  constructor(params) {}
+  constructor(params) {
+    this.tagName = "button";
+
+    this.id = "skipToMain";
+
+    this["data-component"] = "skipToMain";
+
+    this.tabIndex = 0;
+
+    this.class = "btn primary";
+
+    this.textContent = "Skip to Main Content";
+  }
 }
